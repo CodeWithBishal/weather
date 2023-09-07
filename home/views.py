f55ff9a0857c67ea008aaf698dd89245173767cd
Bishal <69814108+CodeWithBishal@users.noreply.github.com>
2023-09-07 20:30:38 +0530

    update

--- home/views.py
+++ home/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from .models import *
+from joblib import load
+
+model = load("./Model/model.joblib")
+
 
 # Create your views here.
 def index(request):
+    if request.method == "POST":
+        city = request.POST['city']
+        date = request.POST['date']
+        prediction = model.predict([[city, date]])
+        context={"pred":prediction}
+        return render(request,"index.html",context=context)
     return render(request,"index.html",)
