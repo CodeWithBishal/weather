b076caf0d9b69ecb70bfd93ecf6658cc4422fb9f
Bishal <69814108+CodeWithBishal@users.noreply.github.com>
2023-09-11 18:10:58 +0530

    a

--- weather/wsgi.py
+++ weather/wsgi.py
@@ -7,9 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
-import os
-
+import os,sys
 from django.core.wsgi import get_wsgi_application
+sys.path.append('/var/www/weather')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weather.settings')
 
