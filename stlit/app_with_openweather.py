116533242827a7f14325f06e5baaaaf7916b4e70
Bishal <69814108+CodeWithBishal@users.noreply.github.com>
2023-09-14 23:38:19 +0530

    update

--- stlit/app_with_openweather.py
+++ stlit/app_with_openweather.py
@@ -38,7 +38,7 @@ def main():
     # fetching the input data from the user
     city = st.text_input('Enter the city:')
     today_date = datetime.today().strftime('%Y-%m-%d')
-    date = st.code(today_date, language="markdown")
+    date = st.text_input(today_date)
 
     # prediction part
     aqi = ''
