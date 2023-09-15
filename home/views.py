08989ee76d22c1c00d3423da2e346b01edf756e4
Bishal <69814108+CodeWithBishal@users.noreply.github.com>
2023-09-15 19:42:51 +0530

    added api

--- home/views.py
+++ home/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from .models import *
 from joblib import load
 import pandas as pd
+import pickle
 
+with open('/var/www/weather/stlit/pakka_final_model.pkl', 'rb') as model_file:
+    loaded_model = pickle.load(model_file)
 # model = load("./Model/model.joblib")
 historical_data = pd.read_csv('/var/www/weather/Model/output.csv')
 
@@ -14,32 +17,30 @@ def index(request):
         print(city)
         # date=date
         print(date)
-        pred = predict_aqi(city, date, historical_data)
+        pred = predict_aqi(city, date, loaded_model, historical_data)
         context={"pred":pred, "date":date, "city":city}
         return render(request,"index.html",context=context)
     return render(request,"index.html",)
 
 from prophet import Prophet
 import matplotlib.pyplot as plt
-def predict_aqi(city, input_date, df):
+def predict_aqi(city, input_date, model, df):
     # Filter data for the specified city
     city_df = df[df['City'] == city].copy()
 
-    #city_df['ds'] = pd.to_datetime(city_df['ds'])
+    # Ensure 'ds' column is in datetime format
+    city_df['ds'] = pd.to_datetime(city_df['ds'])
 
     # Rename columns to 'ds' and 'y' as required by Prophet
     city_df = city_df.rename(columns={'ds': 'ds', 'y': 'y'})
 
-    model = Prophet()
-    model.fit(city_df)
-
     # Create a dataframe with the input date
-    future = pd.DataFrame({'ds': [input_date]})
+    future = pd.DataFrame({'ds': [pd.to_datetime(input_date)]})
 
     # Make predictions for the input date
     forecast = model.predict(future)
 
-
+    # Extract the predicted AQI value
     predicted_aqi = forecast.loc[0, 'yhat']
 
     return predicted_aqi
