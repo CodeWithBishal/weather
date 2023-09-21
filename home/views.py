210fdb31bddc03eff8d1a26fc6bdade999f7f336
Bishal <69814108+CodeWithBishal@users.noreply.github.com>
2023-09-21 21:41:28 +0530

    Update views.py

--- home/views.py
+++ home/views.py
@@ -9,27 +9,26 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 from prophet import Prophet
 import matplotlib.pyplot as plt
+with open('/var/www/weather/Model/pakka_final_model.pkl', 'rb') as model_file:
+    loaded_model = pickle.load(model_file)
 historical_data = pd.read_csv("/var/www/weather/Model/latest/experiment.csv")
-def predict_aqi(city, input_date, df):
+def predict_aqi(city, input_date, model, df):
     # Filter data for the specified city
     city_df = df[df['City'] == city].copy()
 
+    # Ensure 'ds' column is in datetime format
     city_df['ds'] = pd.to_datetime(city_df['ds'])
 
     # Rename columns to 'ds' and 'y' as required by Prophet
     city_df = city_df.rename(columns={'ds': 'ds', 'y': 'y'})
 
-    global model 
-    model = Prophet()
-    model.fit(df)
-
     # Create a dataframe with the input date
-    future = pd.DataFrame({'ds': [input_date]})
+    future = pd.DataFrame({'ds': [pd.to_datetime(input_date)]})
 
     # Make predictions for the input date
     forecast = model.predict(future)
 
-
+    # Extract the predicted AQI value
     predicted_aqi = forecast.loc[0, 'yhat']
 
     return predicted_aqi
@@ -42,7 +41,7 @@ def index(request):
         post_data = json.loads(request.body)
         city = post_data["city"]
         date = post_data["date"]
-        pred = predict_aqi(city, date, historical_data)
+        pred = predict_aqi(city, date, loaded_model, historical_data)
         pred = abs(pred["yhat"][0])
         label = "undefined"
         # if pred<=50:
