bacd4332b1824820cc5ab37c392d21d99dc1050c
Bishal <69814108+CodeWithBishal@users.noreply.github.com>
2023-09-21 19:31:30 +0530

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -113,7 +113,7 @@ async function aQIforecastApi(city){
                 forecastSection.innerHTML += `
                 <li class="card-item">
                     <div class="icon-wrapper">
-                            <span class="badge aqi-${aqi}" style="position:relative;" title="${module.aqiText[aqi].message}">${module.aqiText[aqi].level}
+                            <span class="badge aqi-${aqi}" style="position:unset;" title="${module.aqiText[aqi].message}">${module.aqiText[aqi].level}
                             </span>
                     </div>
                     <p class="label-1">
